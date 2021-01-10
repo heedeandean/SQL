568a901dc46d4399f07fc97aafb2a388a2126557
heejin um <44691187+heedeandean@users.noreply.github.com>
2021-01-11 03:04:33 +0900

    2021-01-11

--- MySQL/trigger.sql
+++ MySQL/trigger.sql
@@ -1,12 +1,12 @@
 DROP TRIGGER IF EXISTS backUserTbl_DeleteTrg;
 DELIMITER //
 CREATE TRIGGER backUserTbl_DeleteTrg
-	AFTER DELETE
+    AFTER DELETE
     ON userTbl
     FOR EACH ROW
 BEGIN
-	INSERT INTO backup_userTbl VALUES(OLD.userID, OLD.name, OLD.birthYear
-		, OLD.addr, OLD.mobile1, OLD.mobile2, OLD.height, OLD.mDate
+    INSERT INTO backup_userTbl VALUES(OLD.userID, OLD.name, OLD.birthYear
+	, OLD.addr, OLD.mobile1, OLD.mobile2, OLD.height, OLD.mDate
         , '삭제', CURDATE(), CURRENT_USER());
 END //
 DELIMITER ;
@@ -15,12 +15,12 @@ DELIMITER ;
 DROP TRIGGER IF EXISTS userTbl_InsertTrg;
 DELIMITER //
 CREATE TRIGGER userTbl_InsertTrg
-	AFTER INSERT
+    AFTER INSERT
     ON userTbl
     FOR EACH ROW
 BEGIN
-	SIGNAL SQLSTATE '45000'
-		SET MESSAGE_TEXT = 'INSERT 안됩니다.';
+    SIGNAL SQLSTATE '45000'
+	SET MESSAGE_TEXT = 'INSERT 안됩니다.';
 END //
 DELIMITER ;
 
@@ -28,14 +28,14 @@ DELIMITER ;
 DROP TRIGGER IF EXISTS userTbl_BeforeInsertTrg;
 DELIMITER //
 CREATE TRIGGER userTbl_BeforeInsertTrg
-	BEFORE INSERT
+    BEFORE INSERT
     ON userTbl
     FOR EACH ROW
 BEGIN
-	IF (NEW.birthYear < 1900) THEN 
-		SET NEW.birthYear = 0;
+    IF (NEW.birthYear < 1900) THEN 
+	SET NEW.birthYear = 0;
     ELSEIF (NEW.birthYear > YEAR(CURDATE())) THEN 
-		SET NEW.birthYear = YEAR(CURDATE());
+	SET NEW.birthYear = YEAR(CURDATE());
     END IF;
 END //
 DELIMITER ;
