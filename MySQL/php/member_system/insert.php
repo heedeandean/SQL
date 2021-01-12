b86a72ea9c4c6d4bb370c13d600d02c59d7e863c
heejin um <44691187+heedeandean@users.noreply.github.com>
2021-01-13 04:56:39 +0900

    2021-01-13

--- MySQL/php/member_system/insert.php
+++ MySQL/php/member_system/insert.php
@@ -3,7 +3,7 @@
     <META http-equiv="content-type" content="text/html" charset="utf-8">
 </HEAD>
 <BODY>
-	<h1> 신규 회원 입력 </h1>
+    <h1> 신규 회원 입력 </h1>
 	
     <FORM METHOD="post" ACTION="insert_result.php">
 		아이디 : <INPUT TYPE="text" NAME="userID"> <br>
@@ -13,7 +13,7 @@
 		휴대폰 국번 : <INPUT TYPE="text" NAME="mobile1"> <br>
 		휴대폰 전화번호 : <INPUT TYPE="text" NAME="mobile2"> <br>
 		신장 : <INPUT TYPE="text" NAME="height"> <br><br><br>
-        <INPUT TYPE="submit" VALUE="회원 입력">        
+        	<INPUT TYPE="submit" VALUE="회원 입력">        
     </FORM>
 </BODY>
 </HTML>
