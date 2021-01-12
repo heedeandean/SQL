9e15079acfaa298e6fb9a1463b64e426be18f54d
heedeandean <7942ahj@hanmail.net>
2021-01-12 10:31:37 +0900

    2021-01-12

--- MySQL/php/for_while.php
+++ MySQL/php/for_while.php
@@ -1,15 +1,22 @@
 <?php
 	$total = 0;
+	$i=123;
+	while($i<=456) {
+		$total += $i;	
+		$i+=2;
+	}
+	echo $total, "<br>";
+
+	$total = 0;
 	for($i=123; $i<=456; $i+=2) {
 		$total += $i;		
 	}
 	echo $total, "<br>";
 
 	$total = 0;
-	$i=123;
-	while($i<=456) {
-		$total += $i;	
-		$i+=2;
+	$newArray = range(1,10);
+	for($i=0; $i<count($newArray); $i++) {
+		$total += $newArray[$i];
 	}
 	echo $total;
 ?>
